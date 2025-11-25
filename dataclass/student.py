c5ef1454a1f258ee222fd8a85f44fd698bfb8940
prajeenabhlon <bhlonprajeena@gmail.com>
2025-11-25 14:14:38 +0545

    dataclass

--- dataclass/student.py
+++ dataclass/student.py
@@ -6,6 +6,8 @@ class student:
   age: int
   grade: int 
   marks: list
+  average : float
+  status : bool
 
   def calculate_avg(self):
     l = len(self.marks)
@@ -13,17 +15,20 @@ class student:
     self.sum = 0
     for i in range(l-1):
       self.sum += self.marks[i]
-
-    print(f"average marks: {self.sum/l}")
+    self.average = self.sum / l
+    print(f"average marks: {self.average}")
 
   def passed(self)->bool:
     l = len(self.marks)
     average = self.sum / l
     if average >= 40:
+      self.status = True
       return True
     else:
+      self.status = False
       return False
-
+  def __str__(self):
+    return f"Student(name = {self.name} , age={self.age}, grade={self.grade} , average:{self.average} , status: {self.status})"
 
 name = input("enter student name: ")
 age = int(input("enter age: "))
@@ -39,11 +44,14 @@ eng = int(input("english marks: "))
 mark_list.append(eng)
 social = int(input("social marks: "))
 mark_list.append(social)
-s = student(name , age , grade , mark_list)
-print(s)
+avg = 0
+stat = None
+s = student(name , age , grade , mark_list , avg , stat)
 s.calculate_avg()
 result = s.passed()
 if result:
   print("pass")
 else:
   print("fail")
+
+print(s)
