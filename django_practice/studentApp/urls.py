24605b45fe33071bf24ea85aec57ec54993187be
prajeenabhlon <bhlonprajeena@gmail.com>
2025-11-30 12:29:27 +0545

    CRUD operation

--- django_practice/studentApp/urls.py
+++ django_practice/studentApp/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('Home/' , views.start_app , name="start_app")
+  path('Home/' , views.home),
+  path('addStudent/' , views.start_app , name="start_app"),
+  path('api/students/' , views.student_api),
+  path('displayStudent/' , views.display ),
+  path('api/students/delete/<int:id>/', views.delete_student_api, name="delete-student-api"),
+  path('deleteStudent/' ,  views.delete)
 ]
--- django_practice/studentApp/views.py
+++ django_practice/studentApp/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render, redirect
 from .models import Student
+from django.http import JsonResponse
 
 # Create your views here.
+def home(request):
+  return render(request , "Home.html")
+
 def start_app(request):
   if request.method == 'POST':
     name = request.POST.get('name')
     age = request.POST.get('age')
-    if name and age:
-      student = Student(name=name, age=age)
+    grade = request.POST.get("grade")
+    if name and age and grade:
+      student = Student(name=name, age=age , grade = grade)
       student.save()
       return redirect('start_app') 
 
-  return render(request , 'Home.html')
+  return render(request , 'addStudent.html')
 
+
+def student_api(request):
+  students = Student.objects.all().values()
+  return JsonResponse(list(students) , safe=False)
+
+def display(request):
+  return render(request , "displayStudent.html")
+
+def delete(request):
+  return render(request , "deleteStudent.html")
+
+
+def delete_student_api(request, id):
+  if request.method == "POST":  
+    try:
+      student = Student.objects.get(id=id)
+      student.delete()
+      return JsonResponse({"message": "Student deleted successfully!"})
+    except Student.DoesNotExist:
+      return JsonResponse({"message": "Student not found!"}, status=404)
